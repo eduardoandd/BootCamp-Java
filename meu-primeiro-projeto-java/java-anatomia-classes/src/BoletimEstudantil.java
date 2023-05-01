c7ee9a4df8ecbc71071d6aa9baf339c20c1fc3c9
eduardoandd <eduardo.andrade.dev@gmail.com>
2023-04-30 21:20:32 -0300

    Anatomia de classe 5 - Identação

---
+++ meu-primeiro-projeto-java/java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,21 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+
+        int mediaFinal = 10;
+
+        if(mediaFinal < 6){
+            System.out.println("Reprovado");
+        }
+
+        else if(mediaFinal == 6){
+            System.out.println("Passou na sorte");
+        }
+        else if(mediaFinal == 10){
+            System.out.println("GABARITOU");
+        }
+        else{
+            System.out.println("APROVADO");
+        }
+        
+    }
+}
